4a0240046bb953eb442d32ce45b25129afd8eebc
NinaLeven <bumblebee504@gmail.com>
2017-08-19 20:08:34 +0300

    Create authorization_handler.js

---
+++ auth/authorization_handler.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const verify_login = require('../util/verifier').verify_login;
+const verify_password = require('../util/verifier').verify_password;
+const sm = require('../util/session_manager');
+
+const handler = (request, response, data, session) => {
+	console.log(sm.get_database());
+	
+	let login = '', password = '';
+	
+	try {
+		login = data.split('$')[0];
+		password = data.split('$')[1];
+	} catch (e) {
+		authorization_error(response);
+		return;
+	}
+	
+	if (verify_login(login) && verify_password(password) && sm.pair_lookup(login, password, sm.get_database())) {
+		sm.add_user(sm.new_user(data.split('$')[0], data.split('$')[1], request.connection.remoteAddress), session);
+		
+		response.writeHead(200, { "content-type" : "text/plain" });
+		response.write('../extern/test.html');
+		response.end();
+	}
+	else {
+		authorization_error(response);
+	}	
+}
+
+const session_check = (login, password) => {	
+	if (verify_login(login) && verify_password(password) && sm.pair_lookup(login, password, sm.get_database())) {		
+		response.writeHead(200, { "content-type" : "text/plain" });
+		response.write('../extern/test.html');
+		response.end();
+	}
+	else {
+		authorization_error(response);
+	}
+}
+
+const authorization_error = (response) => {
+	console.log('authorization error');
+	response.writeHead(401, { "content-type" : "text/plain" });
+	//response.write('http://127.0.0.1:80/authorization');
+	response.write('authorization');
+	response.end();
+}
+
+module.exports.handler = handler;
+module.exports.session_check = session_check;
