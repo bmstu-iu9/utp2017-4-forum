16f12094bc4d32489144f16a5df91b9c5d05de8d
Alexander Drugakov <alexander.drugakov@gmail.com>
2017-08-28 15:20:48 +0300

    Update file_manager.js

--- file_system/file_manager.js
+++ file_system/file_manager.js
@@ -14,6 +14,16 @@ const read_file = (path) => {
 	return fs.readFileSync(path, 'utf8');;
 }
 
+const delete_file = (path) => path {
+	fs.unlinkSync(path);
+}
+
+const modify_file = (path, template, str) => {
+	write_file(path, read_file(path).replace(template, str));
+}
+
+module.exports.modify_file = modify_file
+module.exports.delete_file = delete_file;
 module.exports.append_file = append_file;
 module.exports.write_file = write_file;
 module.exports.read_file = read_file;
