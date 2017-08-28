3c04da4a334bc242afc254a49a354df30ab90272
NinaLeven <bumblebee504@gmail.com>
2017-08-28 23:08:10 +0300

    Update session_manager.js

--- util/session_manager.js
+++ util/session_manager.js
@@ -6,23 +6,26 @@ const get_password = (login) => {
 	let password = '';
 	
 	get_database().some( (elem) => {
-		if (elem.user == login)
+		console.log(elem);
+		if (elem.login.localeCompare(login) == 0)
 			password = elem.password;
-		return elem.user == login;
+		return elem.login.localeCompare(login) == 0;
 	});
 	
-	return password == '' ? undefined : password;
+	return password;
 }
 
 const get_database = () => {
-	return fm.read_file('../auth/secret.txt').split('\n').map( (elem) => { 
+	return fm.read_file('../auth/secret.txt').split('\n').filter( (elem) => {
+		return (elem != undefined && elem.localeCompare('') != 0 && elem.localeCompare('\r') != 0 && elem.lastIndexOf('$') != -1); 
+		}).map( (elem) => { 
 		return { login : elem.split('$')[0], password : elem.split('$')[1].replace('\r', '') };
 	});
 }
 
 const pair_lookup = (login, password, database) => {
 	return database.some( (elem) => { 
-		return elem.login == login && elem.password == password;
+		return elem.login.localeCompare(login) == 0 && elem.password.localeCompare(password) == 0;
 	});
 }
 
@@ -37,16 +40,32 @@ const new_user = (login, password, id) => {
 
 const add_user = (user, session) => {
 	if (!session.some( (elem) => { 
-		if (elem.user == user.user)
+		if (elem.login == user.user)
 			elem = user;
-		return elem.user == user.user
+		return elem.login == user.user
 	})) {
 		session.push(user);
 	}
 }
 
+const is_unregistered = (login, password, database) => {
+	let f = false;
+	
+	f |= database.some( (elem) => { 
+		return elem.login == login;
+	});
+	
+	return !f;
+}
+
+const register = (login, password) => {
+	fm.append_file('../auth/secret.txt', login + '$' + password + '\n');
+}
+
 let database = get_database();
 
+module.exports.register = register;
+module.exports.is_unregistered = is_unregistered;
 module.exports.get_password = get_password;
 module.exports.pair_lookup = pair_lookup;
 module.exports.new_user = new_user;
